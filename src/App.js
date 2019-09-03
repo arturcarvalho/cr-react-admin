c40d0b649a94469f4fa12c91a11214dcb93e8114
Artur Carvalho <arca@gomspace.com>
2019-09-03 13:43:14 +0200

    Change material-ui import to make the builds faster
    https://material-ui.com/guides/minimizing-bundle-size/#option-2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
-
+// Icons slow down the build unless you change the process.
+// https://material-ui.com/guides/minimizing-bundle-size/#option-2
+// For now, the import below is only for testing if it's slower.
+// import { Add, DeleteForever, FourK } from '@material-ui/icons';
 import "./App.css";
 import { UserList, UserEdit, UserCreate } from "./users.js";
 
